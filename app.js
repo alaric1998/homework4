207dfc41785638f57045d3a557bedddd2d427799
alaric1998 <97566981+alaric1998@users.noreply.github.com>
2022-02-15 20:17:37 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let number = 0;
+let start=0;
+let answer="";
+
+
+//for loop to add to number variable
+// for(let i = 10; i>6; i--){
+// number=number+2;
+// }
+
+//while loop like a for loop
+// while(start<4){
+//    number=number+2;
+//    start++;
+//
+// }
+
+// while(answer!=="*"){
+//     answer=prompt("What symbol would you like?")
+
+document.body.innerHTML=Math.floor(Math.random() * 10)+1;
